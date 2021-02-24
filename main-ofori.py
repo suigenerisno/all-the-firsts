a1baa0688af0883831e0f63d88aba91cc0a29ac3
anika ofori <79384634+suigenerisno@users.noreply.github.com>
2021-02-24 12:48:19 -0600

    Add files via upload

---
+++ main-ofori.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 24 11:50:59 2021
+
+@author: suigenerisno
+"""
+
+# this is my first python program for pirple assignment
+#     suigenerisno
+print("My favorite Song- My First Python Program")
+a = "We "
+b = "Are "
+c = "The "
+d = "World "
+
+print (a + b + c + d)
+
+print ("Produced by Quincy Jones and Michael Jackson")
+
+print (" and was written by")
+
+print ("Michael Jackson and Lionel Richie")
+print ("in")
+a = 1900
+b = 85
+print (a + b)
+
+genre = "Pop"
+DurationInSeconds = 431
+print ("Genre is ")
+print (genre)
+print ("Length of song in Seconds is")
+print (DurationInSeconds)
+
+"""
+In 1985 We Are The World won a Grammy
+and MTV VMA for Record and Video of the Year
+"""
+# Lionel Richie told Rolling Stone all the juicy details of the making
